de4416c9649beeab8e84301fa382788214aa96e6
jake <priddyjacob84@gmail.com>
2025-07-02 08:21:07 -0400

    updated 2D map, added demo controls

---
+++ Assets/UserMovement.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class UserMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(h, 0, v);
+        transform.Translate(move * moveSpeed * Time.deltaTime, Space.World);
+    }
+}
